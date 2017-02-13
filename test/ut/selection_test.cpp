3922996e0701f28b4a34936c930d1f4816f226a0
Jakub Turowski <tk7077@gmail.com>
2017-02-13 21:21:08 +0100

    Added Selection and PolygonSelection UTs

---
+++ test/ut/selection_test.cpp
@@ -0,0 +1,29 @@
+#include <gtest/gtest.h>
+#include <selection.hpp>
+
+struct selectionTest : public testing::Test
+{
+    Selection sut;
+};
+
+TEST_F(selectionTest, select)
+{
+    sut.selectAll(3);
+    EXPECT_TRUE(sut.contains(0));
+    EXPECT_TRUE(sut.contains(1));
+    EXPECT_TRUE(sut.contains(2));
+    EXPECT_FALSE(sut.contains(3));
+    sut.select(4);
+    EXPECT_TRUE(sut.contains(4));
+}
+
+TEST_F(selectionTest, unselect)
+{
+    sut.selectAll(3);
+    sut.unselect(1);
+    EXPECT_TRUE(sut.contains(0));
+    EXPECT_FALSE(sut.contains(1));
+    EXPECT_TRUE(sut.contains(2));
+    sut.unselectAll();
+    EXPECT_TRUE(sut.empty());
+}
